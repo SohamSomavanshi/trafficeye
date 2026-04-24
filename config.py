d6f30dd48e5fa189f9209ca59160d84ac5e50028
SohamSomavanshi <sohamsms11@gmail.com>
2026-04-24 07:51:01 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# ─────────────────────────────────────────
+#  Traffic Eye — Email Configuration
+#  Fill in your details below
+# ─────────────────────────────────────────
+
+EMAIL        = "notss1106@gmail.com"   # 
+APP_PASSWORD = "hjer qaeh oypd dpog"    # Your Gmail App Password (see README)
